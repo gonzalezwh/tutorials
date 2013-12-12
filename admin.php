01190f6f3cbfa41d610725a8a75c238dc235f15d
William Gonzalez <whg@pdx.edu>
2013-12-12 12:23:26 -0800

    Final steps on Ex 3

--- admin.php
+++ admin.php
@@ -1,12 +1,14 @@
 <?php
-
-if(!empty($_GET['user'])){
-    $error=0;
-    $SESSION['username']=$_GET['user'];
-    $username=$SESSION['username'];
-if (!$conn=mysqli_connect("tutorials.local.10.0.0.10.nip.io",$username))
+session_start();
+$error=0;
+if (!isset($_SESSION['user'])) {
+    die('User name does not have access! ');
+}
+$username=$_SESSION["user"];
+if(!empty($username)){
+    if (!$conn=mysqli_connect("tutorials.local.10.0.0.10.nip.io",$username))
     {
-        echo "Failed to connect to mysql ".mysql_connect_error();
+        echo "failed to connect to mysql ".mysql_connect_error();
     }
 
     if (!mysqli_select_db($conn,"tutorials")){
@@ -24,16 +26,18 @@ if (!$conn=mysqli_connect("tutorials.local.10.0.0.10.nip.io",$username))
             }
         }
 }
+unset ($_SESSION['user']);
 ?>
 <html>
 <html>
 <head>
 <body>
     <H2>Email Report</H2>
+    <H2><?php echo "Usuario: ".$username ?></H2>
     <?php if($error): ?>
         <H2>There was an error in your report. Please correct it.</H2>
     <?php endif; ?>
-    <TABLE>
+    <TABLE BORDERS=1>
         <TR>
             <TH>Id</TH>
             <TH>Name</TH>
@@ -41,15 +45,15 @@ if (!$conn=mysqli_connect("tutorials.local.10.0.0.10.nip.io",$username))
             <TH>Subject</TH>
             <TH>Headers</TH>
         </TR>
-        <?php while($row = mysqli_fetch_array($result)) {
-       echo " <TR>";
-       echo "<TD>".htmlspecialchars($row['id'])."</TD>";
-       echo "<TD>".htmlspecialchars($row['to_whom'])."</TD>";
-       echo "<TD>".htmlspecialchars($row['subject'])."</TD>";
-       echo "<TD>".htmlspecialchars($row['textmsg'])."</TD>";
-       echo "<TD>".htmlspecialchars($row['headers'])."</TD>";
-       echo "</TR>";
-        } ?>
+<?php while($row = mysqli_fetch_array($result)) {
+    echo " <TR>";
+    echo "<TD>".htmlspecialchars($row['id'])."</TD>";
+    echo "<TD>".htmlspecialchars($row['to_whom'])."</TD>";
+    echo "<TD>".htmlspecialchars($row['subject'])."</TD>";
+    echo "<TD>".htmlspecialchars($row['textmsg'])."</TD>";
+    echo "<TD>".htmlspecialchars($row['headers'])."</TD>";
+    echo "</TR>";
+} ?>
     </table>
 </body>
 </head>
--- login.php
+++ login.php
@@ -1,24 +1,13 @@
 <?php
-if (!isset($_GET['service'])){
-print_r($_GET);
+session_start();
 $ticket=$_GET['ticket'];
 $ticket=substr($ticket,strpos($ticket,'>'));
 $ticket=substr($ticket,0,strlen($ticket));
 $ticket=trim($ticket);
 $ticket=utf8_encode($ticket);
 $service=utf8_encode("http://10.0.0.10/login.php");
-$_SESSION['xml']="https://sso.pdx.edu/cas/proxyValidate?ticket=".$ticket."&service=".$service;
-$output=file_get_contents($_SESSION['xml']);
-$xml = new SimpleXMLElement($output);
-print_r($output);
-//header("HTTP/1.1 301 Moved Permanently");
-//header("Location: http://10.0.0.10/admin.php?user=".$xml->user);
-}
+$url='https://sso.pdx.edu/cas/proxyValidate?ticket='.$ticket.'&service='.$service;
+$output=file_get_contents($url);
+if (strpos($output,"whg")>0) $_SESSION["user"] = "whg";
+header("Location: http://10.0.0.10/admin.php");
 ?>
-<html>
-<head>
-<body>
-    <a href=<?php echo $_SESSION['xml'] ?>>validate</a>
-</body>
-</head>
-</html>
